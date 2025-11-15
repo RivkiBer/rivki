1c104a350a67544a5701a2ad494adc0458b6a9f4
RivkiBer <rivki3959@gmail.com>
2025-11-15 19:57:07 +0200

    Add files via upload

---
+++ script.py
@@ -0,0 +1,8 @@
+import os
+from fileinput import close
+
+# os.mkdir("C:\\Users\\PC\\Desktop\\Python\\PyCharmMiscProject\\folder")
+# os.rmdir("C:\\Users\\PC\\Desktop\\Python\\PyCharmMiscProject\\folder")
+with open('C:\\Users\\PC\\Desktop\\Python\\PyCharmMiscProject\\folder\\file.txt','w')as file:
+    file.write("Hello World")
+
