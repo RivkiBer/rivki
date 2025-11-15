250536b199e54c83f69f6a2aa26bc2d438bbb8f1
RivkiBer <rivki3959@gmail.com>
2025-11-15 19:58:37 +0200

    Update script.py

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ from fileinput import close
 # os.mkdir("C:\\Users\\PC\\Desktop\\Python\\PyCharmMiscProject\\folder")
 # os.rmdir("C:\\Users\\PC\\Desktop\\Python\\PyCharmMiscProject\\folder")
 with open('C:\\Users\\PC\\Desktop\\Python\\PyCharmMiscProject\\folder\\file.txt','w')as file:
-    file.write("Helo World")
+    file.write("Hello World")
+
 
 
